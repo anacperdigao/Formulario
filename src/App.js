3c7acef8804750ce1b873e04502a50b2a885dc52
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-17 16:31:51 -0300

    Dividindo semanticamente em 2 sections card e form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <section className='container__cards'>
+        <p>cards</p>
+      </section>
       
+      <section className='container__formulario'>
+        <p>formulario</p>
+      </section>
     </div>
   );
 }
